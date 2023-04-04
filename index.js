8af7e946a4937361196aca0bd2707196449603dd
Jubayth Hossen Roni <84420078+rony9843@users.noreply.github.com>
2023-04-04 09:49:32 +0300

    this is server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 2000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// app.post("/signUp", (req, res) => {
+//   console.log(req);
+//   res.send("successful");
+// });
+
+app.post("/signUp", (req, res) => {
+  console.log(req.body);
+  res.send("success");
+});
+app.get("/signUp", (req, res) => {
+  res.send("success 123");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
