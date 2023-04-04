361910d806d724a667b6f841df50194e9a6a24aa
Jubayth Hossen Roni <84420078+rony9843@users.noreply.github.com>
2023-04-04 10:23:42 +0300

    config update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 2000 || process.env.PORT;
+require("dotenv").config();
+
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require("dotenv").config();
+const port = 2000 || process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cors());
